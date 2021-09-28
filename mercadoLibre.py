b75f48c95d5188fcb094124ac360b11e9597ed39
Your Name <jose@rpamaker.com>
2021-09-27 23:32:10 -0300

    primer cambio

--- mercadoLibre.py
+++ mercadoLibre.py
@@ -1,1 +1,14 @@
 import unittest
+from unittest.case import TestCase
+from selenium import webdriver
+import selenium
+from selenium.common.exceptions import NoAlertPresentException
+
+class MercadoLibre(unittest,TestCase):
+
+    def setUp(self):
+        self.driver =webdriver.Chrome()
+        driver = self.driver
+        driver.implicitly_wait(30)
+        driver.maximize_window()
+        driver.get()
